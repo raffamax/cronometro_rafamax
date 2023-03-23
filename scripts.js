6bdc54e3b6ee3f7a2b5314249e289e8544f35557
Rafa Max <rafaelmaxguitar@gmail.com>
2023-03-23 00:53:52 -0300

    funcional ok

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 // Variáveis globais
 let timerInterval = null; // Intervalo do cronômetro
+let lapInterval = null; // Intervalo da volta atual
 let running = false; // Indica se o cronômetro está rodando
 let time = 0; // Tempo total
+let currentlaptime = 0; // Tempo da volta atual
 let lapTimes = []; // Tempos de volta
+let currentlapNumber = 1;
 
 
 // Função para formatar o tempo em hh:mm:ss:ms
@@ -21,17 +24,33 @@ function updateTimer() {
   document.getElementById('timer').textContent = formatTime(time);
 }
 
+// Função para formatar o tempo da volta atual
+function formatLap(currentlaptime){
+  let laphours = Math.floor(currentlaptime / 360000);
+  let lapminutes = Math.floor((currentlaptime % 360000) / 6000);
+  let lapseconds = Math.floor((currentlaptime % 6000) / 100);
+  let lapmilliseconds = currentlaptime % 100;
+  return `${laphours.toString().padStart(2, '0')}:${lapminutes.toString().padStart(2, '0')}:${lapseconds.toString().padStart(2, '0')}:${lapmilliseconds.toString().padStart(2, '0')}`;
+}
+
+function updatelap() {
+  currentlaptime++;
+  document.getElementById('currentlap').innerHTML = `Lap ${currentlapNumber} -> ` + formatLap(currentlaptime);
+}
+
 
 // Função para iniciar ou parar o cronômetro
 function startStop() {
   if (running) {
     clearInterval(timerInterval);
+    clearInterval(lapInterval);
     running = false;
     document.getElementById('startStopButton').textContent = 'Start';
     document.getElementById('lapButton').style.display = 'none';
     document.getElementById('resetButton').style.display = 'inline'; // adiciona estilo display:inline para o botão Reset
   } else {
     timerInterval = setInterval(updateTimer, 10);
+    lapInterval = setInterval(updatelap, 10);
     running = true;
     document.getElementById('startStopButton').textContent = 'Stop';
     document.getElementById('lapButton').style.display = 'inline';
@@ -42,10 +61,14 @@ function startStop() {
 // Função para resetar o cronômetro
 function reset() {
   clearInterval(timerInterval);
+  clearInterval(lapInterval);
   running = false;
   time = 0;
+  currentlaptime = 0;
+  currentlapNumber = 1;
   lapTimes = [];
-  document.getElementById('timer').textContent = '00:00:00';
+  document.getElementById('timer').textContent = '00:00:00:00';
+  document.getElementById('currentlap').innerHTML = '';
   document.getElementById('laps').innerHTML = '';
   document.getElementById('startStopButton').textContent = 'Start';
   document.getElementById('resetButton').style.display = 'none'; // adiciona estilo display:none novamente ao botão Reset
@@ -53,6 +76,8 @@ function reset() {
 
 // Função para adicionar uma volta
 function lap() {
+  currentlaptime = 0;
+  currentlapNumber ++;
   let lapTime = time;
   if (lapTimes.length > 0) {
     lapTime -= lapTimes[lapTimes.length - 1];
