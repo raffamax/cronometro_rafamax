b46958b90e0af5cb1d0d7e3f66cca1f060f381f1
Rafa Max <rafaelmaxguitar@gmail.com>
2023-03-24 01:14:46 -0300

    some styles

--- scripts.js
+++ scripts.js
@@ -1,14 +1,12 @@
-// Variáveis globais
-let timerInterval = null; // Intervalo do cronômetro
-let lapInterval = null; // Intervalo da volta atual
-let running = false; // Indica se o cronômetro está rodando
-let time = 0; // Tempo total
-let currentlaptime = 0; // Tempo da volta atual
-let lapTimes = []; // Tempos de volta
+let timerInterval = null; 
+let lapInterval = null; 
+let running = false; 
+let time = 0; 
+let currentlaptime = 0;
+let lapTimes = [];
 let currentlapNumber = 1;
 
 
-// Função para formatar o tempo em hh:mm:ss:ms
 function formatTime(time) {
   let hours = Math.floor(time / 360000);
   let minutes = Math.floor((time % 360000) / 6000);
@@ -17,14 +15,11 @@ function formatTime(time) {
   return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${milliseconds.toString().padStart(2, '0')}`;
 }
 
-
-// Função para atualizar o cronômetro a cada segundo
 function updateTimer() {
   time++;
   document.getElementById('timer').textContent = formatTime(time);
 }
 
-// Função para formatar o tempo da volta atual
 function formatLap(currentlaptime){
   let laphours = Math.floor(currentlaptime / 360000);
   let lapminutes = Math.floor((currentlaptime % 360000) / 6000);
@@ -35,30 +30,43 @@ function formatLap(currentlaptime){
 
 function updatelap() {
   currentlaptime++;
-  document.getElementById('currentlap').innerHTML = `Lap ${currentlapNumber} -> ` + formatLap(currentlaptime);
+  document.getElementById('currentlap').innerHTML = `Lap ${currentlapNumber.toString().padStart(2, '0')} -> ` + formatLap(currentlaptime);
 }
 
-
-// Função para iniciar ou parar o cronômetro
 function startStop() {
   if (running) {
     clearInterval(timerInterval);
     clearInterval(lapInterval);
     running = false;
     document.getElementById('startStopButton').textContent = 'Start';
+    document.getElementById('startStopButton').style = 'background-color: green;';
     document.getElementById('lapButton').style.display = 'none';
-    document.getElementById('resetButton').style.display = 'inline'; // adiciona estilo display:inline para o botão Reset
+    document.getElementById('resetButton').style.display = 'inline';
   } else {
     timerInterval = setInterval(updateTimer, 10);
     lapInterval = setInterval(updatelap, 10);
     running = true;
     document.getElementById('startStopButton').textContent = 'Stop';
+    document.getElementById('startStopButton').style = 'background-color: red;';
     document.getElementById('lapButton').style.display = 'inline';
-    document.getElementById('resetButton').style.display = 'none'; // remove estilo display:block do botão Reset
+    document.getElementById('resetButton').style.display = 'none';
   }
 }
 
-// Função para resetar o cronômetro
+function lap() {
+  currentlaptime = 0;
+  currentlapNumber ++;
+  let lapTime = time;
+  if (lapTimes.length > 0) {
+    lapTime -= lapTimes[lapTimes.length - 1];
+  }
+  lapTimes.push(time);
+  let lapNumber = lapTimes.length;
+  let lapDiv = document.createElement('div');
+  lapDiv.textContent = `Lap ${lapNumber.toString().padStart(2, '0')} ->  ${formatTime(lapTime)}`;
+  document.getElementById('laps').prepend(lapDiv);
+}
+
 function reset() {
   clearInterval(timerInterval);
   clearInterval(lapInterval);
@@ -71,22 +79,5 @@ function reset() {
   document.getElementById('currentlap').innerHTML = '';
   document.getElementById('laps').innerHTML = '';
   document.getElementById('startStopButton').textContent = 'Start';
-  document.getElementById('resetButton').style.display = 'none'; // adiciona estilo display:none novamente ao botão Reset
+  document.getElementById('resetButton').style.display = 'none';
 }
-
-// Função para adicionar uma volta
-function lap() {
-  currentlaptime = 0;
-  currentlapNumber ++;
-  let lapTime = time;
-  if (lapTimes.length > 0) {
-    lapTime -= lapTimes[lapTimes.length - 1];
-  }
-  lapTimes.push(time);
-  let lapNumber = lapTimes.length;
-  let lapDiv = document.createElement('div');
-  lapDiv.textContent = `Lap ${lapNumber} -> ${formatTime(lapTime)}`;
-  document.getElementById('laps').prepend(lapDiv);
-}
-
-
